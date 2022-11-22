313337653e56e8f069651899f24aec8b2c723bfa
Zachary Slate <49423173+ZSlate@users.noreply.github.com>
2022-11-22 11:13:38 -0800

    Main Menu Updates
    Added options button and scene. Created title on main menu. Some alignment issue fixes.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -15,4 +15,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Level_1");
     }
+
+    public void optionsButton()
+    {
+        SceneManager.LoadScene("Options Menu");
+    }
 }
