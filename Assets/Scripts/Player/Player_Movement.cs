7e66086745a7a6a540dfe34336e88f26814a1315
Emanuel-N <80660934+Emanuel-N@users.noreply.github.com>
2022-11-23 14:42:06 -0800

    Added A jump Multiplier
    Made the jump feel less floaty - EmanuelN

--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -4,73 +4,97 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    [Header("Movement Settings")]
     public float speed;
-    public float jumpforce;
+    private bool facingRight = true;
     private float horizontalInput;
 
-    private bool right = true; 
-    private Rigidbody2D rb;
-
-    private bool isGrounded;
+    [Header("Jump Settings")]
     public Transform groundCheck;
-    public float checkRadius;
     public LayerMask Ground;
+    public float jumpforce;
+    public int extraJumps;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    private float checkRadius = 0.5f;
+    private bool isGrounded;
+    
+    
+    
 
-    public int extraJumps; 
-
-
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();  
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
-    
-    // Use for any physics related aspects
-    private void FixedUpdate()
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        Movement();
+        CheckArea();
+        Jump();
+
+    }
+
+    void CheckArea()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, Ground);
-        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    void Movement()
+    {
 
         rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
 
-        // moving left
-        if (right == false && horizontalInput > 0)
+
+        if (facingRight == false && horizontalInput > 0)
         {
-            Flip(); 
+            Flip();
         }
-        // moving right 
-        else if(right == true && horizontalInput < 0)
+
+        else if (facingRight == true && horizontalInput < 0)
         {
             Flip();
         }
 
     }
-    // Update is called once per frame
-    void Update()
+
+    void Jump()
     {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
         if (isGrounded == true)
         {
-            extraJumps = 1; 
+            extraJumps = 1;
         }
         if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
         {
             rb.velocity = Vector2.up * jumpforce;
-            extraJumps--; 
+            extraJumps--;
         }
-        else if(Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
         {
             rb.velocity = Vector2.up * jumpforce;
         }
-    }
 
+    }
     void Flip()
     {
-        right = !right;
+        facingRight = !facingRight;
         Vector3 temp = transform.localScale;
         temp.x = temp.x * -1;
-        transform.localScale = temp; 
+        transform.localScale = temp;
 
 
     }
