ae97d5cd779ca7375c6dac8be242e943d77fe574
Zachary Slate <zslate1@gmail.com>
2023-02-16 11:47:00 -0800

    added death panel.
    just need to link it to the health system

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject DeathPanel;
+
+    public void ToggleDeathPanel()
+    {
+        DeathPanel.SetActive(!DeathPanel.activeSelf);
+    }
+}
