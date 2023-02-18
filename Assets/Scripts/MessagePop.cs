e76e0ecdffb8593984f9be6e6baf98cd25d12e99
allisonrosej <70971185+allisonrosej@users.noreply.github.com>
2023-02-17 16:57:52 -0800

    Added  more things

---
+++ Assets/Scripts/MessagePop.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessagePop : MonoBehaviour
+{
+
+    public GameObject Message;
+
+    private void OnTriggerEnter2D( Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Message.SetActive(true)
+        }
+
+
+    }
+
+    private void OnTriggerExit2D( Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Message.SetActive(false)
+        }
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
