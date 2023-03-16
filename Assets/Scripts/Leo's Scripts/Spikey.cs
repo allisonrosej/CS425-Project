82a5a9698b4570c07cc9cca3c231c94e9a304afb
leogalang <leogalang@nevada.unr.edu>
2023-03-16 16:43:22 -0700

    Spikes
    Still working on object reference error

---
+++ Assets/Scripts/Leo's Scripts/Spikey.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikey : MonoBehaviour
+{
+
+    Health playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealth = GameObject.Find("health").GetComponent<Health>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject whatHit = col.gameObject;
+        if (whatHit.CompareTag("Player"))
+        {
+            this.playerHealth.TakeDamage(5);
+        }
+    }
+
+}
