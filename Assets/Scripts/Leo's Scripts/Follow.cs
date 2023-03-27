d9b49b7d5d1c85745214178429c64f9cb44f4917
leogalang <leogalang@nevada.unr.edu>
2023-03-27 01:01:19 -0700

    Ghost
    Ghost follows player once in specified range of player. Still needs sprite, attack, and damage

---
+++ Assets/Scripts/Leo's Scripts/Follow.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+
+    public GameObject target;
+    public float ratio = 0.05f;
+    bool foundPlayer;
+    public float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if(target == null)
+            target = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (!foundPlayer)
+        {
+
+            Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, range);
+
+            foreach (Collider2D h in hits)
+                if (h.gameObject == target)
+                {
+                    foundPlayer = true;
+
+                    break;
+                }
+
+        }
+
+
+        if (foundPlayer)
+        {
+
+            float step;
+
+            if (Vector3.Dot(target.transform.position - transform.position, target.transform.localScale.x * Vector3.right) > 0)
+            {
+                step = ratio * 2;
+            }
+            else
+            {
+                step = ratio;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+}
