d9b194057d93392605090caba3dc033834fc2146
leogalang <leogalang@nevada.unr.edu>
2023-04-20 16:53:01 -0700

    Mage
    Ball damage

---
+++ Assets/Scripts/Leo's Scripts/Shoot 2.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    private float timer;
+    private Animator anim;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if(distance < 19)
+        {
+            timer += Time.deltaTime;
+
+
+
+
+
+            if (timer > 1)
+            {
+
+
+
+                timer = 0;
+
+
+                  
+                shoot();
+            }
+
+        }
+
+    }
+
+    void shoot()
+    {
+        anim.SetTrigger("Attack3");
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+
+
+
+}
